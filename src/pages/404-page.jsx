05a1e72d839e677a846a65f240bbe81e30f812f8
Diky-Without-C <snayzher@gmail.com>
2024-03-14 18:35:18 +0700

    fix: link 404 page

--- src/pages/404-page.jsx
+++ src/pages/404-page.jsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <div className="flex h-full w-full flex-col items-center justify-center">
         <h1 className="text-3xl font-bold">404</h1>
         <p className="text-xl">this page not found</p>
-        <Link to="/" className="font-bold text-blue-600">
+        <Link to="/AnimeIn/" className="font-bold text-blue-600">
           Back to home
         </Link>
       </div>
